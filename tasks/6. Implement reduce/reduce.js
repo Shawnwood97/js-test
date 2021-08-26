2590c2fd1a8ac598376e22e8bfde5c18755684b1
Shawn Wood <Shawnwood97@gmail.com>
2021-08-26 02:28:00 -0600

    completed reduce challenge

--- tasks/6. Implement reduce/reduce.js
+++ tasks/6. Implement reduce/reduce.js
@@ -1,3 +1,7 @@
 export function Reduce(array, reducer, initialValue) {
-  // TODO: write your code here
+  //* I wasn't immediately familiar with the reduce() method, but a quick google search got me there, not super complicated given this implementation.
+
+  //* Since the majority of the logic is included in the test file, all we need to do, is use the reduce() method on the passed array, and pass the
+  //* supplied reducer and initalValue as arguments, which will add or concat depending on which test we are on (str or numbers)
+  return array.reduce(reducer, initialValue);
 }
